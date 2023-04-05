7541e09d4c96e10d56911346361fa33ad1b1ded1
Harshal <harshalpatilself@gmail.com>
2023-04-05 21:48:03 +0530

    Added: New cpp Files

---
+++ Addition.cpp
@@ -0,0 +1,26 @@
+// NAME : HARSHAL PATIL
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+
+    cout<<"\n Enter First No : ";
+    cin>>a;
+
+    cout<<"\n Enter Second No : ";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<"\n Sum is "<<c;   
+    
+    return 0;
+}
+
+/* Enter First No : 5
+
+ Enter Second No : 9
+
+ Sum is 14*/
